366fde5ca805378f5a644d8410291453459c3edf
t2pellet <t2pellet@uwaterloo.ca>
2023-12-05 22:57:41 -0500

    fix: no cors

--- src/main/kotlin/com/t2pellet/boycottisraelapi/BoycottConfig.kt
+++ src/main/kotlin/com/t2pellet/boycottisraelapi/BoycottConfig.kt
@@ -5,12 +5,17 @@ import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableCaching
 class BoycottConfig: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedMethods("*")
+    }
 
     override fun configurePathMatch(configurer: PathMatchConfigurer) {
         configurer.setUseTrailingSlashMatch(true)
